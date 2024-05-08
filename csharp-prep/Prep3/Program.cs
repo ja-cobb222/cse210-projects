8f6f202c00ca17c1610a9f44116324ba0ff08cfe
Jacob <jacobisaksen19@gmail.com>
2024-05-08 00:07:18 -0700

    Completed Prep3 Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        int guess = -1;
+
+        Console.WriteLine("A random number between 1 and 10 has been selected. ");
+        
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+    
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
+
     }
 }
