6f2db9f1f77c56c9e8c54238c01c7afaef43f6bb
Jacob <jacobisaksen19@gmail.com>
2024-05-07 19:29:13 -0700

    Completed Prep2 assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        if (percent >= 90)
+        {
+            Console.WriteLine("Your grade is an A. ");
+        }
+
+        else if (percent >= 80 && percent < 90)
+        {
+            Console.WriteLine("Your grade is a B. ");
+        }
+
+        else if (percent >= 70 && percent < 80)
+        {
+            Console.WriteLine("Your grade is a C. ");
+        }
+
+        else if (percent >= 60 && percent < 70)
+        {
+            Console.WriteLine("Your grade is a D. ");
+        }
+        
+        else if (percent >= 0 && percent < 60)
+        {
+            Console.WriteLine("Your grade is an F. ");
+        }
+
     }
 }
