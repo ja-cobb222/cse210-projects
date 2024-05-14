843d73fc31cc800482c650993b5205090dfd678b
Jacob <jacobisaksen19@gmail.com>
2024-05-13 22:07:20 -0700

    Completed Learning2 assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Senior Software Engineer";
+        job1._company = "Mighty Coconut";
+        job1._startYear = 2030;
+        job1._endYear = 2050;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Microsoft";
+        job2._startYear = 2025;
+        job2._endYear = 2030;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Isaac Jacobsen";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
